e538247fb2fbea48b52e3f63b8bc87301c5b6a9d
KoppoluManeesha <maneeshakoppolu@gmail.com>
2026-04-03 22:49:42 +0530

    added env variables support

--- finance_backend/settings.py
+++ finance_backend/settings.py
@@ -21,12 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@956i8e1if1mib08m!nu+b_k9qe4$eh6st8td3naluj)*cb_zh'
+SECRET_KEY = os.environ.get("SECRET_KEY", "fallback-secret")
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",")
 
 
 # Application definition
